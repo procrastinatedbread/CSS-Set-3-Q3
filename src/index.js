c9dca32e065ae87d2a53831d1eedbf1a463ff58f
Nikhil Chhabra <91406971+procrastinatedbread@users.noreply.github.com>
2023-06-23 15:36:05 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+
+<div class="card">
+<h1>This is a Heading</h1>
+<h3>This is a subheading</h3>
+<img src="https://picsum.photos/500/500">
+<p>You have to add other CSS properties on your own and make your card look closest to the image shown below</p>
+
+</div>
+`;
